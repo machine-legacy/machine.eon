a34975374d37121415e85f526825991fbfc8b99c
Jacob Lewallen <jlewallen@gmail.com>
2008-10-05 11:23:00 -0700

    Starting on the Listener, for transforming into our model.
    Right now we just grab stuff via the repositories, no actual references
    have been added just yet.

--- Source/Machine.Eon/Mapping/Listener.cs
+++ Source/Machine.Eon/Mapping/Listener.cs
@@ -1,14 +1,25 @@
 using System;
 using System.Collections.Generic;
 
+using Machine.Eon.Mapping.Repositories;
+using Machine.Eon.Mapping.Repositories.Impl;
+
 namespace Machine.Eon.Mapping
 {
   public class Listener
   {
+    private readonly IMethodRepository _methodRepository = new MethodRepository();
+    private readonly ITypeRepository _typeRepository = new TypeRepository();
     private readonly Stack<AssemblyName> _assemblies = new Stack<AssemblyName>();
     private readonly Stack<NamespaceName> _namespaces = new Stack<NamespaceName>();
     private readonly Stack<TypeName> _types = new Stack<TypeName>();
     private readonly Stack<MethodName> _methods = new Stack<MethodName>();
+
+    public Listener()
+    {
+      _types.Push(TypeName.None);
+      _methods.Push(MethodName.None);
+    }
 
     public void StartAssembly(AssemblyName name)
     {
@@ -32,6 +43,22 @@ namespace Machine.Eon.Mapping
 
     public void UseType(TypeName name)
     {
+      AssemblyName assemblyName = _assemblies.Peek();
+      TypeName typeName = _types.Peek();
+      MethodName methodName = _methods.Peek();
+      if (typeName == TypeName.None)
+      {
+        return;
+      }
+      if (methodName != MethodName.None)
+      {
+        Method method = _methodRepository.FindMethod(assemblyName, methodName);
+      }
+      else
+      {
+        Type type = _typeRepository.FindType(assemblyName, typeName);
+      }
+      Console.WriteLine("{0} {1} {2} {3}", assemblyName, typeName.Name, methodName, name);
     }
 
     public void EndMethod()
