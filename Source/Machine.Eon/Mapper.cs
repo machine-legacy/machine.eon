9ae33ec50f5f9fa80fa8b53c0e2e8f669a5f83e8
Jacob Lewallen <jlewallen@gmail.com>
2008-10-30 16:55:55 -0700

    Some useful diagnostic code when we're generating the QueryRoot.
    Going to maybe do some lazy loading of 3rd party stuff here.

--- Source/Machine.Eon/Mapper.cs
+++ Source/Machine.Eon/Mapper.cs
@@ -36,7 +36,46 @@ namespace Machine.Eon
 
     public QueryRoot ToQueryRoot()
     {
+      CheckForInvalidTypes();
       return new QueryRoot(_assemblyRepository.FindAll());
+    }
+
+    public void CheckForInvalidTypes()
+    {
+      var invalids = from assembly in _assemblyRepository.FindAll()
+                     from ns in assembly.Namespaces
+                     from type in ns.Types
+                     where type.IsPendingCreation
+                     select type;
+      foreach (Type type in invalids.Distinct())
+      {
+        _log.Info("Invalid: " + type);
+      }
+
+      var assemblies = from assembly in _assemblyRepository.FindAll()
+                       where assembly.Name.Name.Equals("Machine.Eon.Specs")
+                       select assembly;
+      
+      var methods = from assembly in assemblies
+                    from ns in assembly.Namespaces
+                    from type in ns.Types
+                    from method in type.Methods
+                    where method.IsPendingCreation
+                    select method;
+      foreach (Method method in methods)
+      {
+        _log.Info("Invalid: " + method);
+      }
+
+      var types = from assembly in assemblies
+                  from ns in assembly.Namespaces
+                  from type in ns.Types
+                  where ns.Name.Name.Equals("Machine.Eon.Specs.ClassesAndInterfaces")
+                  select type;
+      foreach (Type type in types)
+      {
+        _log.Info(type);
+      }
     }
   }
 }
