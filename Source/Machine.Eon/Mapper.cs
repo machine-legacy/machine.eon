c4c29ba17a674ba937f5b313e2919f635293303d
Jacob Lewallen <jlewallen@gmail.com>
2008-11-05 17:23:49 -0800

    Working on getting external assemblies loading.
    This introduces a PendingTypeLoader that iterates over assemblies that
    have pending types and tries to load their AssemblyDefinition's. That's
    pretty much it right now. We try and do GAC loads and then we iterate
    over paths we got the other assemblies from, using .dll and .exe to find
    them physically on disk (if GAC fails)

--- Source/Machine.Eon/Mapper.cs
+++ Source/Machine.Eon/Mapper.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Mono.Cecil;
 
+using Machine.Eon.Mapping;
 using Machine.Eon.Mapping.Inspection;
 using Machine.Eon.Mapping.Repositories;
 using Machine.Eon.Mapping.Repositories.Impl;
+using Type = Machine.Eon.Mapping.Type;
 
 namespace Machine.Eon
 {
   public class Mapper
   {
     private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(typeof(Mapper));
+    private readonly List<string> _directories = new List<string>();
     private readonly IAssemblyRepository _assemblyRepository;
     private readonly ITypeRepository _typeRepository;
     private readonly IMemberRepository _memberRepository;
@@ -25,6 +29,11 @@ namespace Machine.Eon
 
     public void Include(string path)
     {
+      string directory = System.IO.Path.GetDirectoryName(path);
+      if (!_directories.Contains(directory))
+      {
+        _directories.Add(directory);
+      }
       AssemblyDefinition definition = AssemblyFactory.GetAssembly(path);
       ModelCreator modelCreator = new ModelCreator(_typeRepository, _memberRepository);
       MyReflectionStructureVisitor visitor = new MyReflectionStructureVisitor(modelCreator);
@@ -33,7 +42,82 @@ namespace Machine.Eon
 
     public QueryRoot ToQueryRoot()
     {
+      PendingTypeLoader pendingTypeLoader = new PendingTypeLoader(_directories);
+      pendingTypeLoader.Load(_assemblyRepository.FindAll());
       return new QueryRoot(_assemblyRepository.FindAll());
+    }
+  }
+
+  public class PendingTypeLoader
+  {
+    private readonly ExternalAssemblyLoader _externalAssemblyLoader;
+
+    public PendingTypeLoader(List<string> directories)
+    {
+      _externalAssemblyLoader = new ExternalAssemblyLoader(directories);
+    }
+
+    public void Load(IEnumerable<Assembly> assemblies)
+    {
+      foreach (AssemblyDefinition definition in _externalAssemblyLoader.FindExternalAssemblyDefinitions(assemblies))
+      {
+      }
+    }
+  }
+
+  public class ExternalAssemblyLoader
+  {
+    private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(typeof(PendingTypeLoader));
+    private readonly List<string> _directories;
+
+    public ExternalAssemblyLoader(List<string> directories)
+    {
+      _directories = directories;
+    }
+
+    public IEnumerable<AssemblyDefinition> FindExternalAssemblyDefinitions(IEnumerable<Assembly> assemblies)
+    {
+      var pending = from assembly in assemblies from type in assembly.Types where type.IsPending select assembly;
+      foreach (Assembly assembly in pending.Distinct())
+      {
+        AssemblyDefinition definition = GetAssemblyDefinition(assembly);
+        if (definition == null)
+        {
+          _log.Warn("Not able to load: " + assembly.Key);
+        }
+        yield return definition;
+      }
+    }
+
+    private AssemblyDefinition GetAssemblyDefinition(Assembly assembly)
+    {
+      try
+      {
+        System.Reflection.Assembly dotNetAssembly = System.Reflection.Assembly.ReflectionOnlyLoad(assembly.Key.FullName);
+        return AssemblyFactory.GetAssembly(dotNetAssembly.Location);
+      }
+      catch (Exception error)
+      {
+        return GetAssemblyDefinitionFromPath(assembly);
+      }
+    }
+
+    private AssemblyDefinition GetAssemblyDefinitionFromPath(Assembly assembly)
+    {
+      string[] extensions = new string[]  { ".dll", ".exe" };
+      foreach (string directory in _directories)
+      {
+        foreach (string extension in extensions)
+        {
+          string filename = System.IO.Path.Combine(directory, assembly.Key.Name + extension);
+          if (System.IO.File.Exists(filename))
+          {
+            System.Reflection.Assembly dotNetAssembly = System.Reflection.Assembly.ReflectionOnlyLoadFrom(filename);
+            return AssemblyFactory.GetAssembly(dotNetAssembly.Location);
+          }
+        }
+      }
+      return null;
     }
   }
 }
